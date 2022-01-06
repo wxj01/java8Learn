1154623862b2d5c1cd9e5648b5b7ef025bfd9d0d
wxj_2021 <wangxinjian_01@163.com>
2022-01-06 11:19:43 +0800

    java stream

--- src/test/java/com/wxj/java8learn/stream/demo2/TestStream.java
+++ src/test/java/com/wxj/java8learn/stream/demo2/TestStream.java
@@ -253,4 +253,204 @@ public class TestStream {
         System.out.println("工资之和：" + sumSalary.get() + "," + sumSalary2 + "," + sumSalary3);
         System.out.println("最高工资：" + maxSalary + "," + maxSalary2);
     }
+
+    /**
+     * @description: TODO 收集(collect) 归集(toList/toSet/toMap)
+     * @author wangxinjian
+     * @date 2022/1/6 0006 9:07
+     * @version 1.0
+     */
+    @Test
+    public void test12(){
+        List<Integer> list = Arrays.asList(1, 6, 3, 4, 6, 7, 9, 6, 20);
+        List<Integer> listNew = list.stream().filter(x -> x % 2 == 0).collect(Collectors.toList());
+        Set<Integer> set = list.stream().filter(x -> x % 2 == 0).collect(Collectors.toSet());
+
+        List<Person> personList = getPersonList();
+        Map<String, Person> map = personList.stream().filter(p -> p.getSalary() > 8000)
+                .collect(Collectors.toMap(Person::getName, p -> p));
+
+        System.out.println("toList:" + listNew);
+        System.out.println("toSet:" + set);
+        System.out.println("toMap:" + map);
+
+    }
+
+    /**
+     * @description: TODO 统计(count/averaging)
+     * @author wangxinjian
+     * @date 2022/1/6 0006 9:11
+     * @version 1.0
+     */
+    @Test
+    public void test13(){
+        List<Person> personList = new ArrayList<Person>();
+        personList.add(new Person("Tom", 8900, 23, "male", "New York"));
+        personList.add(new Person("Jack", 7000, 25, "male", "Washington"));
+        personList.add(new Person("Lily", 7800, 21, "female", "Washington"));
+
+        //求人数总和
+        Long count = personList.stream().collect(Collectors.counting());
+        //求平均工资
+        Double average = personList.stream().collect(Collectors.averagingDouble(Person::getSalary));
+        //求最高工资
+        Optional<Integer> max = personList.stream().map(Person::getSalary).collect(Collectors.maxBy(Integer::compareTo));
+        //最高工资2
+        Optional<Person> max2 = personList.stream().collect(Collectors.maxBy((p1, p2) -> Integer.compare(p1.getSalary(), p2.getSalary())));
+        //求工资之和
+        Integer sum = personList.stream().collect(Collectors.summingInt(Person::getSalary));
+        //员工工资所有统计
+        DoubleSummaryStatistics collect = personList.stream().collect(Collectors.summarizingDouble(Person::getSalary));
+
+
+        System.out.println("员工总数：" + count);
+        System.out.println("员工平均工资：" + average);
+        System.out.println("员工max工资：" + max.get());
+        System.out.println("员工max2工资：" + max2.get().getSalary());
+        System.out.println("员工工资总和：" + sum);
+        System.out.println("员工工资所有统计：" + collect);
+
+
+    }
+
+    /**
+     * @description: TODO 分组(partitioningBy/groupingBy)
+     * @author wangxinjian
+     * @date 2022/1/6 0006 9:32
+     * @version 1.0
+     */
+    @Test
+    public void test14(){
+        List<Person> personList = getPersonList();
+
+        //将员工按薪资是否高于8000分组
+        Map<Boolean, List<Person>> part = personList.stream().collect(Collectors.partitioningBy(x -> x.getSalary() > 8000));
+        //将员工按性别分组
+        Map<String, List<Person>> group = personList.stream().collect(Collectors.groupingBy(Person::getSex));
+        //将员工先按性别分组，再按地区分组
+        Map<String, Map<String, List<Person>>> group2 = personList.stream()
+                .collect(Collectors.groupingBy(Person::getSex, Collectors.groupingBy(Person::getArea)));
+
+        System.out.println("员工按薪资是否大于8000分组情况：" + part);
+        System.out.println("员工按性别分组情况：" + group);
+        System.out.println("员工按性别、地区：" + group2);
+
+    }
+
+    /**
+     * @description: TODO 接合(joining)
+     * @author wangxinjian
+     * @date 2022/1/6 0006 9:42
+     * @version 1.0
+     */
+    @Test
+    public void test15(){
+        List<Person> personList = getPersonList();
+        String names = personList.stream().map(Person::getName).collect(Collectors.joining("<*>"));
+        System.out.println("所有员工的姓名:"+names);
+
+        List<String> list = Arrays.asList("A", "B", "C");
+        String string = list.stream().collect(Collectors.joining("$"));
+        System.out.println("拼接后的字符串：" + string);
+    }
+
+    /**
+     * @description: TODO 归约(reducing)
+     * @author wangxinjian
+     * @date 2022/1/6 0006 9:47
+     * @version 1.0
+     */
+    @Test
+    public void test16(){
+        List<Person> personList = getPersonList();
+
+        // 每个员工减去起征点后的薪资之和（这个例子并不严谨，但一时没想到好的例子）
+        Optional<Integer> reduce = personList.stream().map(person -> person.getSalary() - 5000).reduce(Integer::sum);
+        System.out.println("员工扣税薪资总和：" + reduce.get());
+
+
+        Integer sum = personList.stream().collect(Collectors.reducing(0, Person::getSalary, (i, j) -> (i + j - 5000)));
+        System.out.println("员工扣税薪资总和：" + sum);
+
+        // stream的reduce
+        Optional<Integer> sum2 = personList.stream().map(Person::getSalary).reduce(Integer::sum);
+        System.out.println("员工薪资总和：" + sum2.get());
+
+    }
+
+    /**
+     * @description: TODO  排序(sorted)
+     * @author wangxinjian
+     * @date 2022/1/6 0006 10:05
+     * @version 1.0
+     */
+    @Test
+    public void test17(){
+        List<Person> personList = getPersonList();
+        // 按工资升序排序（自然排序）
+        List<String> newList = personList.stream()
+                .sorted(Comparator.comparing(Person::getSalary))
+                .map(Person::getName)
+                .collect(Collectors.toList());
+
+        // 按工资倒序排序
+        List<String> newList2 = personList.stream()
+                .sorted(Comparator.comparing(Person::getSalary).reversed())
+                .map(Person::getName)
+                .collect(Collectors.toList());
+
+        //先按工资再按年龄升序
+        List<String> newList3 = personList.stream()
+                .sorted(Comparator.comparing(Person::getSalary)
+                        .thenComparing(Person::getAge))
+//                .sorted(Comparator.comparing(Person::getAge))
+                .map(Person::getName)
+                .collect(Collectors.toList());
+
+        // 先按工资再按年龄自定义排序（降序）
+        List<String> newList4 = personList.stream().sorted((p1, p2) -> {
+            if (p1.getSalary() == p2.getSalary()) {
+                return p2.getAge() - p1.getAge();
+            } else {
+                return p2.getSalary() - p2.getSalary();
+            }
+        }).map(Person::getName)
+                .collect(Collectors.toList());
+
+        System.out.println("按工资升序排序：" + newList);
+        System.out.println("按工资降序排序：" + newList2);
+        System.out.println("先按工资再按年龄升序排序：" + newList3);
+        System.out.println("先按工资再按年龄自定义降序排序：" + newList4);
+
+
+    }
+
+    /**
+     * @description: TODO 提取/组合
+     * @author wangxinjian
+     * @date 2022/1/6 0006 10:22
+     * @version 1.0
+     */
+    @Test
+    public void test18(){
+        String[] arr1 = { "a", "b", "c", "d" };
+        String[] arr2 = { "d", "e", "f", "g" };
+
+        Stream<String> stream1 = Stream.of(arr1);
+        Stream<String> stream2 = Stream.of(arr2);
+
+        // concat:合并两个流 distinct：去重
+        List<String> newList = Stream.concat(stream1, stream2).distinct().collect(Collectors.toList());
+        // limit：限制从流中获得前n个数据
+        List<Integer> collect = Stream.iterate(1, x -> x + 2).limit(10).collect(Collectors.toList());
+
+        // skip：跳过前n个数据
+        List<Integer> collect1 = Stream.iterate(1, x -> x + 2).skip(1).limit(5).collect(Collectors.toList());
+
+        System.out.println("流合并：" + newList);
+        System.out.println("limit：" + collect);
+        System.out.println("skip：" + collect1);
+
+    }
+
 }
