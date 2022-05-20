65bc05bde1a0b2bedb13c058444761b36a96d27b
wxj01 <wangxinjian_01@163.com>
2022-05-20 09:48:37 +0800

    测试

---
+++ src/test/java/com/wxj/java8learn/test.java
@@ -0,0 +1,11 @@
+package com.wxj.java8learn;
+
+/**
+ * @author wxj
+ * @version 1.0.0
+ * @ClassName test.java
+ * @Description TODO
+ * @createTime 2022年05月20日 09:47:00
+ */
+public class test {
+}
