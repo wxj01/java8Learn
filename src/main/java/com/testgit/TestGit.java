fd0976ac208f28a212958269dab196cc273449f2
wxj01 <wangxinjian_01@163.com>
2022-06-28 21:54:05 +0800

    '测试git001'

---
+++ src/main/java/com/testgit/TestGit.java
@@ -0,0 +1,14 @@
+package com.testgit;
+
+/**
+ * @author wxj
+ * @version 1.0.0
+ * @ClassName TestGit.java
+ * @Description TODO
+ * @createTime 2022年06月28日 21:52:00
+ */
+public class TestGit {
+    public static void main(String[] args) {
+
+    }
+}
